79aa6dda5c347c70382d86106ecebb0bab03aebf
gr3gburk3 <greg.burke51690@gmail.com>
2020-01-06 16:56:15 -0500

    Second Commit, Working on Game Logic
    I'm working on event listeners/handlers but have become a little
    stuck

--- assets/scripts/board/events.js
+++ assets/scripts/board/events.js
@@ -1,20 +1,64 @@
 'use strict'
-// const gameboard = ['', '', '', '', '', '', '', '', '']
-// let playerX = true
-// let playerO = false
+const playerX = true
+const gameOver = false
 
 const playZero = function () {
-  $('#zero').on('click', changeText)
+  $('#zero').on('click', changeValue)
+  console.log('O')
+}
+const playOne = function () {
+  $('#one').on('click', changeValue)
+}
+const playTwo = function () {
+  $('#two').on('click', changeValue)
+  console.log('X')
+}
+const playThree = function () {
+  $('#three').on('click', changeValue)
+}
+const playFour = function () {
+  $('four').on('click', changeValue)
+}
+const playFive = function () {
+  $('five').on('click', changeValue)
+}
+const playSix = function () {
+  $('six').on('click', changeValue)
+}
+const playSeven = function () {
+  $('seven').on('click', changeValue)
+}
+const playEight = function () {
+  $('four').on('click', changeValue)
 }
 
-const changeText = (event) => {
-  $(`#zero`).text('x')
-}
+const changeValue = () => {
+  if (playerX === true && !gameOver === false) {
+    $(`#${this.id}`).text('X')
+  } else {
+    $(`#${this.id}`).text('O')
+  }
+/*
+  const playOne = function () {
+    $('#one').on('click', changeOne)
+  }
 
-const addHandlers = () => {
-  playZero()
-}
-
-module.exports = {
-  addHandlers
+  const changeOne = (event) => {
+    $(`#one`).text('x')
+  }
+*/
+  const addHandlers = () => {
+    playZero()
+    playOne()
+    playTwo()
+    playThree()
+    playFour()
+    playFive()
+    playSix()
+    playSeven()
+    playEight()
+  }
+  module.exports = {
+    addHandlers
+  }
 }
